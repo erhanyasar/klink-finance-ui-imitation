31c6b2a33c2db2e4a14fd257eff5a1e7ea67e6d4
Erhan Yaşar <mail@erhanyasar.com.tr>
2025-03-13 18:28:27 +0300

    feat: adds assets and sets config for remotePatterns of animations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'klink-cdn.klink.finance',
+        port: '',
+        pathname: '/ecosystem/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
